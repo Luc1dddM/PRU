ceabe252cdca30e4f1de01d4d3b0404627dd2223
NET_02_NguyenSonLam <lamns2593@gmail.com>
2024-05-19 21:22:53 +0700

    Fix bug

--- Assets/Scripts/Player/Move.cs
+++ Assets/Scripts/Player/Move.cs
@@ -12,12 +12,15 @@ public class Move : MonoBehaviour
     public bool isGrounded;
     private Rigidbody2D rb;
     public LayerMask groundMask;
+    private Animator animator;
+    public bool facingRight = true;
 
     public bool canJump = true;
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = gameObject.GetComponent<Animator>();
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
@@ -28,18 +31,18 @@ public class Move : MonoBehaviour
 
         if(jumpSpeed == 0.0f && isGrounded)
         {
+           CheckFacingDirection(moveInput);
+            animator.SetFloat("Movement", Mathf.Abs(rb.velocity.x));
             rb.velocity = new Vector2(moveInput * walkSpeed, rb.velocity.y);
 
         }
 
-
-        isGrounded = Physics2D.OverlapBox(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 1.2f),
+        isGrounded = Physics2D.OverlapBox(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.72f),
             new Vector2(1.12f, 0.30f), 0f, groundMask);
 
         if (Input.GetKey(KeyCode.Space) && isGrounded && canJump)
         {
-            jumpSpeed += 0.6f;
-
+            jumpSpeed += Time.deltaTime * 70f;
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded && canJump)
@@ -77,16 +80,26 @@ public class Move : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawCube(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 1.2f), new Vector2(1.12f, 0.20f));
+        Gizmos.DrawCube(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.72f), new Vector2(1.12f, 0.20f));
     }
 
-    /*    // OnCollisionEnter2D is called when this object collides with another object
-        private void OnCollisionEnter2D(Collision2D collision)
+    private void CheckFacingDirection(float horizontalInput)
+    {
+        if (facingRight && horizontalInput < 0f)
         {
-            // Check if the player collides with a ground object (e.g., platform, floor)
-            if (collision.gameObject.CompareTag("Ground"))
-            {
-                isGrounded = true; // Player is grounded
-            }
-        }*/
+            Flip();
+        }
+        else if (!facingRight && horizontalInput > 0f)
+        {
+            Flip();
+        }
+    }
+    private void Flip()
+    {
+        var playerScale = transform.localScale;
+        transform.localScale = new Vector3(playerScale.x * -1, playerScale.y, playerScale.z);
+        facingRight = !facingRight;
+    }
+
+
 }
