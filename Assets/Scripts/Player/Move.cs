f6e5881fe59c881225719199224b5e451eddb571
StarBui3010 <saobcce170038@fpt.edu.vn>
2024-06-02 17:47:44 +0700

    5 redmap and double jump script

--- Assets/Scripts/Player/Move.cs
+++ Assets/Scripts/Player/Move.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,21 +7,31 @@ public class Move : MonoBehaviour
 
     public float walkSpeed = 8f;
     public float jumpSpeed = 0.0f;
+    public float jumpDistance = 2f;
+    public bool isGrounded;
+    public LayerMask groundMask;
+    public bool facingRight = true;
+    public bool canJump = true;
+    public Collider2D bodycollider;
+    public Collider2D Footcollider;
+    public PhysicsMaterial2D normalMa;
+    public PhysicsMaterial2D bounceMa;
+
 
     private float moveInput;
-    public bool isGrounded;
     private Rigidbody2D rb;
-    public LayerMask groundMask;
     private Animator animator;
-    public bool facingRight = true;
+    public bool canDoubleJump; // kiểm tra trạng thái nhảy
 
-    public bool canJump = true;
+
 
     // Start is called before the first frame update
     void Start()
     {
         animator = gameObject.GetComponent<Animator>();
         rb = gameObject.GetComponent<Rigidbody2D>();
+        Footcollider.sharedMaterial = normalMa;
+        rb.gravityScale = 6f;
     }
 
     // Update is called once per frame
@@ -29,41 +39,61 @@ public class Move : MonoBehaviour
     {
         moveInput = Input.GetAxisRaw("Horizontal");
 
-        if(jumpSpeed == 0.0f && isGrounded)
+        if (jumpSpeed == 0.0f && isGrounded)
         {
-           CheckFacingDirection(moveInput);
+
+            CheckFacingDirection(moveInput);
             animator.SetFloat("Movement", Mathf.Abs(rb.velocity.x));
             rb.velocity = new Vector2(moveInput * walkSpeed, rb.velocity.y);
 
         }
 
-        isGrounded = Physics2D.OverlapBox(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.72f),
-            new Vector2(1.12f, 0.30f), 0f, groundMask);
+        if (isGrounded)
+        {
+            bodycollider.sharedMaterial = normalMa;
+            canDoubleJump = false; // Đặt lại trạng thái nhảy khi tiếp đất
+
+        }
+        else
+        {
+            bodycollider.sharedMaterial = bounceMa;
+        }
+
+
+
+
+
+        isGrounded = Physics2D.OverlapBox(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.72f)
+            , new Vector2(1.0f, 0.20f), 0f, groundMask);
 
         if (Input.GetKey(KeyCode.Space) && isGrounded && canJump)
         {
-            jumpSpeed += Time.deltaTime * 70f;
+            jumpSpeed += Time.deltaTime * 60f;
+
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded && canJump)
         {
-            rb.velocity = new Vector2(0.0f,rb.velocity.y);
+            rb.velocity = new Vector2(0.0f, rb.velocity.y);
+            canDoubleJump = true; // Đặt lại trạng thái nhảy khi đang ở trên mặt đất
         }
 
-        if (jumpSpeed >= 30f && isGrounded)
+        if (jumpSpeed >= 27f && isGrounded)
         {
-            float tempx = moveInput * walkSpeed;
+            float tempx = moveInput * walkSpeed * jumpDistance;
             float tempy = jumpSpeed;
+
             rb.velocity = new Vector2(tempx, tempy);
-            Invoke("resetJump", 0.1f);
+            canJump = false;
+            canDoubleJump = true; // Đặt trạng thái nhảy khi nhảy lên
         }
 
         if (Input.GetKeyUp(KeyCode.Space))
         {
             if (isGrounded)
             {
-                rb.velocity = new Vector2(moveInput * walkSpeed, jumpSpeed);
-                jumpSpeed = 0f;
+                rb.velocity = new Vector2(moveInput * walkSpeed * jumpDistance, jumpSpeed);
+                canDoubleJump = true; // Đặt lại trạng thái nhảy khi đang ở trên mặt đất
             }
             canJump = true;
         }
@@ -71,16 +101,34 @@ public class Move : MonoBehaviour
 
     }
 
-    void resetJump()
+
+
+    private void LateUpdate()
     {
-        
-        jumpSpeed = 0.0f;
+        if (!isGrounded)
+        {
+            jumpSpeed = 0f;
+            canDoubleJump = true; // Đặt lại trạng thái nhảy khi đang ở trên mặt đất
+
+        }
+        else
+    {
+            canDoubleJump = false; // Đặt lại trạng thái nhảy khi đang ở trên mặt đất
     }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            canJump = true;
+        }
+    }
+
+
+
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawCube(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.72f), new Vector2(1.12f, 0.20f));
+        Gizmos.DrawCube(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y - 0.72f), new Vector2(1.1f, 0.20f));
     }
 
     private void CheckFacingDirection(float horizontalInput)
