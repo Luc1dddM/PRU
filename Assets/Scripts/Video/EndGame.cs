339bf07ac8c0eb13fca3ca63bdec765f1946e816
tammi03 <tambnmce170642@fpt.edu.vn>
2024-07-07 12:45:30 +0700

    fix video end game, fix map ice

---
+++ Assets/Scripts/Video/EndGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class EndGame : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.loopPointReached += OnVideoEnd; // khi video kết thúc
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        /*DataActionManager.instance.NewGame();*/
+        SceneManager.LoadScene("MainMenu"); // Chuyển đến scene "MainMenu"
+    }
+}
